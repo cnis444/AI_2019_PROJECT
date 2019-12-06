4bb1ba9e05770cf649c54b7910bc2ac08c31ee54
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2019-12-06 18:03:11 +0100

    Building have deformation

---
+++ AI_Project/Assets/Scripts/Building/Area.cs
@@ -0,0 +1,295 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area
+{
+    List<List<Vector2>> vertices;
+    int step;
+
+    public List<Vector2> Main {
+        get {
+            return vertices.Count > 0 ? vertices[0] : new List<Vector2>();
+        }
+    }
+
+    public List<List<Vector2>> Surfaces {
+        get {
+            return vertices;
+        }
+    }
+
+    public Area() {
+        vertices = new List<List<Vector2>>();
+        step = -1;
+    }
+
+    public void Add(RectInt rect) {
+        Add(RectToList(rect));
+    }
+
+    public void Add(List<Vector2> area) {
+        step++;
+        if (vertices.Count == 0) {
+            //Debug.Log(string.Format("Step {0}: first area", step));
+            vertices.Add(area);
+        }
+        else {
+            // How many edges are touching?
+            List<int[]> touchingEdges;
+            int idx = -1;
+            do {
+                idx++;
+                touchingEdges = TouchingEdgesIndex(vertices[idx], area);
+            } while (touchingEdges.Count == 0 && idx < vertices.Count - 1);
+            //Debug.Log(string.Format("Step {0}: {1} touching edges with surface {2}/{3}", step, touchingEdges.Count, idx + 1, vertices.Count));
+
+            switch (touchingEdges.Count) {
+                case 0:
+                    vertices.Add(area);
+                    break;
+                default:
+                    Add(idx, area, touchingEdges);
+                    break;
+            }
+        }
+        //Debug.Log("Step " + step + ": " + (vertices.Count - 1) + " secondary area(s)");
+    }
+
+    private static List<Vector2> RectToList(RectInt rect) {
+        return new List<Vector2> {
+            new Vector2(rect.xMin, rect.yMin),
+            new Vector2(rect.xMax, rect.yMin),
+            new Vector2(rect.xMax, rect.yMax),
+            new Vector2(rect.xMin, rect.yMax)
+        };
+    }
+
+    public static List<Vector2[]> TouchingEdges(RectInt rect1, RectInt rect2) {
+        return TouchingEdges(RectToList(rect1), RectToList(rect2));
+    }
+
+    private static List<Vector2[]> TouchingEdges(List<Vector2> area1, List<Vector2> area2) {
+        List<Vector2[]> list = new List<Vector2[]>();
+        int cpt = 0;
+        Vector2 a, b, c, d;
+        for (int i = 0; i < area1.Count - 1; i++) {
+            a = area1[i];
+            b = area1[i + 1];
+            for (int j = 0; j < area2.Count - 1; j++) {
+                c = area2[j];
+                d = area2[j + 1];
+                if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+                    list.Add(new Vector2[] { a, b, c, d });
+                    cpt++;
+                }
+            }
+            // last edge of area2
+            c = area2[area2.Count - 1];
+            d = area2[0];
+            if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+                list.Add(new Vector2[] { a, b, c, d });
+                cpt++;
+            }
+        }
+        // last edge of area1
+        a = area1[area1.Count - 1];
+        b = area1[0];
+        for (int j = 0; j < area2.Count - 1; j++) {
+            c = area2[j];
+            d = area2[j + 1];
+            if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+                list.Add(new Vector2[] { a, b, c, d });
+                cpt++;
+            }
+        }
+        // last edge of area2
+        c = area2[area2.Count - 1];
+        d = area2[0];
+        if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+            list.Add(new Vector2[] { a, b, c, d });
+            cpt++;
+        }
+
+        return list;
+    }
+
+    private static List<int[]> TouchingEdgesIndex(List<Vector2> area1, List<Vector2> area2) {
+        List<int[]> list = new List<int[]>();
+        int cpt = 0;
+        Vector2 a, b, c, d;
+        for (int i = 0; i < area1.Count - 1; i++) {
+            a = area1[i];
+            b = area1[i + 1];
+            for (int j = 0; j < area2.Count - 1; j++) {
+                c = area2[j];
+                d = area2[j + 1];
+                if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+                    list.Add(new int[] { i, i+1, j, j+1 });
+                    cpt++;
+                }
+            }
+            // last edge of area2
+            c = area2[area2.Count - 1];
+            d = area2[0];
+            if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+                list.Add(new int[] { i, i+1, area2.Count-1, 0 });
+                cpt++;
+            }
+        }
+        // last edge of area1
+        a = area1[area1.Count - 1];
+        b = area1[0];
+        for (int j = 0; j < area2.Count - 1; j++) {
+            c = area2[j];
+            d = area2[j + 1];
+            if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+                list.Add(new int[] { area1.Count-1, 0, j, j+1 });
+                cpt++;
+            }
+        }
+        // last edge of area2
+        c = area2[area2.Count - 1];
+        d = area2[0];
+        if (PointsAreAligned(a, b, c, d) && b != d && a != c && (OrderIs(a, d, b) || OrderIs(d, a, c))) {
+            list.Add(new int[] { area1.Count - 1, 0, area2.Count-1, 0 });
+            cpt++;
+        }
+
+        bool continius = true;
+        for (int i = 0; i < list.Count - 1; i++) {
+            if (list[i][1] != list[i + 1][0]) {
+                continius = false;
+                break;
+            }
+        }
+
+        return continius ? list : new List<int[]>();
+    }
+
+    private static bool PointsAreAligned(Vector2 a, Vector2 b, Vector2 c, Vector2 d) {
+        Vector2 ab = (b - a).normalized;
+        Vector2 ac = (c - a).normalized;
+        Vector2 ad = (d - a).normalized;
+
+        //bool ret = (ab == ac || ab == -ac) && (ab == ad || ab == -ad);
+        bool ret = Vector2.Angle(ab, ac) == 0 && Vector2.Angle(ab, ad) == 0;
+        //Debug.Log(string.Format("{0}, {1}, {2}, {3} {4} aligned ({5}  {6})", a, b, c, d, ret ? "are" : "are not", Vector2.Angle(ab, ac), Vector2.Angle(ab, ad)));
+        return ret;
+    }
+
+    private static bool PointsAreAligned(Vector2 a, Vector2 b, Vector2 c) {
+        Vector2 ab = (b - a).normalized;
+        Vector2 ac = (c - a).normalized;
+
+        return Vector2.Angle(ab, ac) == 0;
+    }
+
+    private static bool OrderIs(Vector2 a, Vector2 b, Vector2 c) {
+        bool ret = ((a.x <= b.x && b.x <= c.x) || (a.x >= b.x && b.x >= c.x)) && 
+            ((a.y <= b.y && b.y <= c.y) || (a.y >= b.y && b.y >= c.y));
+        //Debug.Log(string.Format("Order {0}, {1}, {2} is {3}", a, b, c, ret));
+        return ret;
+    }
+
+    private void Add(int addTo, List<Vector2> area, List<Vector2[]> touchingEdges) {
+        int[] mainIndices = new int[1 + touchingEdges.Count];
+        int[] areaIndices = new int[1 + touchingEdges.Count];
+        mainIndices[0] = vertices[0].IndexOf(touchingEdges[0][0]);
+        areaIndices[0] = area.IndexOf(touchingEdges[0][3]);
+        for (int i = 0; i < touchingEdges.Count; i++) {
+            mainIndices[i + 1] = vertices[0].IndexOf(touchingEdges[i][1]);
+            areaIndices[i + 1] = area.IndexOf(touchingEdges[i][2]);
+        }
+
+        Add(addTo, area, mainIndices, areaIndices);
+    }
+
+    private void Add(int addTo, List<Vector2> area, List<int[]> touchingEdges) {
+        int[] mainIndices = new int[1 + touchingEdges.Count];
+        int[] areaIndices = new int[1 + touchingEdges.Count];
+        mainIndices[0] = touchingEdges[0][0];
+        areaIndices[0] = touchingEdges[0][3];
+        for (int i = 0; i < touchingEdges.Count; i++) {
+            mainIndices[i + 1] = touchingEdges[i][1];
+            areaIndices[i + 1] = touchingEdges[i][2];
+        }
+
+        Add(addTo, area, mainIndices, areaIndices);
+    }
+
+    private void Add(int addTo, List<Vector2> area, int[] mainIndices, int[] areaIndices) {
+
+        // Debug mainIndices
+        string s = mainIndices[0].ToString();
+        for (int i = 1; i < mainIndices.Length; i++) {
+            s += " -> " + mainIndices[i];
+        }
+        Debug.Log(string.Format("Step {0}: mainIndices: {1} ({2} points)", step, s, vertices[addTo].Count));
+
+        // Add points to the main area
+
+        List<Vector2> list = new List<Vector2>();
+        // First point
+        if (area[areaIndices[0]] == vertices[addTo][mainIndices[0]]) {
+            vertices[addTo].RemoveAt(mainIndices[0]);
+            for (int i = 0; i < mainIndices.Length; i++) {
+                if (mainIndices[i] >= mainIndices[0]) {
+                    mainIndices[i]--;
+                }
+            }
+            //Debug.Log(string.Format("Step {0}: Delete first (idx: {1}, pos: {2})", step, areaIndices[0], area[areaIndices[0]]));
+        }
+        else {
+            list.Add(area[areaIndices[0]]);
+            //Debug.Log(string.Format("Step {0}: Add first (idx: {1}, pos: {2})", step, areaIndices[0], area[areaIndices[0]]));
+        }
+        // Intermediate points
+        for (int i = areaIndices[0] + 1; i < area.Count + areaIndices[0] - areaIndices.Length + 1; i++) {
+            list.Add(area[i % area.Count]);
+            //Debug.Log(string.Format("Step {0}: Add intermediate (idx: {1}, pos: {2})", step, i % area.Count, area[i % area.Count]));
+        }
+        // Last point (if different from the first one)
+        if (areaIndices[areaIndices.Length - 1] != areaIndices[0]) {
+            if (area[areaIndices[areaIndices.Length - 1]] == vertices[addTo][mainIndices[mainIndices.Length - 1]]) {
+                vertices[addTo].RemoveAt(mainIndices[mainIndices.Length - 1]);
+                for (int i = 0; i < mainIndices.Length; i++) {
+                    if (mainIndices[i] >= mainIndices[mainIndices.Length - 1]) {
+                        mainIndices[i]--;
+                    }
+                }
+                //Debug.Log(string.Format("Step {0}: Delete last (idx: {1}, pos: {2})", step, areaIndices[areaIndices.Length - 1], area[areaIndices[areaIndices.Length - 1]]));
+            }
+            else {
+                list.Add(area[areaIndices[areaIndices.Length - 1]]);
+                //Debug.Log(string.Format("Step {0}: Add last (idx: {1}, pos: {2})", step, areaIndices[areaIndices.Length - 1], area[areaIndices[areaIndices.Length - 1]]));
+            }
+        }
+        // Points between last and first
+        for (int i = 1; i < areaIndices.Length - 1; i++) {
+            if (area[areaIndices[i]] == vertices[addTo][mainIndices[i]]) {
+                vertices[addTo].RemoveAt(mainIndices[i]);
+                for (int j = 0; j < mainIndices.Length; j++) {
+                    if (mainIndices[j] >= mainIndices[i]) {
+                        mainIndices[j]--;
+                    }
+                }
+                //Debug.Log(string.Format("Step {0}: Delete other (idx: {1}, pos: {2})", step, areaIndices[i], area[areaIndices[i]]));
+            }
+            else {
+                Debug.LogWarning("encountered a point between last and first that do not match it's corresponding point in the main area");
+            }
+        }
+        Debug.Log(string.Format("Step {0}: inserting {1} points at index {2}/{3}", step, list.Count, mainIndices[0] + 1 /*% vertices[addTo].Count*/, vertices[addTo].Count));
+        vertices[addTo].InsertRange((mainIndices[0] + 1)/* % vertices[addTo].Count*/, list);
+
+        // Try to add secondary areas to the main area;
+        if (vertices.Count > 1) {
+            List<List<Vector2>> tmp = vertices.GetRange(1, vertices.Count - 1);
+            vertices.RemoveRange(1, vertices.Count - 1);
+            for (int i = 0; i < tmp.Count; i++) {
+                Add(tmp[i]);
+            }
+        }
+    }
+}
--- AI_Project/Assets/Scripts/Building/Building.cs
+++ AI_Project/Assets/Scripts/Building/Building.cs
@@ -10,8 +10,14 @@ public class Building : MonoBehaviour
     public int width, length, height;
     [Range(0,1)]
     public float windowProbability;
+    public int minPartitionSize, maxPartitionSize;
+    public int RectangleToRemove;
+    public int m;
 
     List<Wall>[] stages;
+    SpacePartition partition;
+    List<RectInt> rects;
+    Area area;
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +27,16 @@ public class Building : MonoBehaviour
         }
         Random.InitState(seed);
 
-        // Init walls
+        partition = new SpacePartition(new RectInt(0, 0, width, length), minPartitionSize, maxPartitionSize, Random.Range(-1000000, 1000000));
+        rects = partition.GetSpaces();
+
+        //InitSpace();
+        //InitWalls();
         stages = new List<Wall>[height];
         for (int k = 0; k < height; k++) {
-            List<Vector3> corners = new List<Vector3>();
-            corners.Add(new Vector3(0, k, 0));
-            corners.Add(new Vector3(width, k, 0));
-            corners.Add(new Vector3(width, k, length));
-            corners.Add(new Vector3(0, k, length));
-
-            stages[k] = new List<Wall>();
-            for (int i = 0; i < corners.Count - 1; i++) {
-                stages[k].Add(new Wall(corners[i], corners[i + 1]));
-            }
-            stages[k].Add(new Wall(corners[corners.Count - 1], corners[0]));
+            RemoveSpace();
+            BuildArea();
+            BuildWall(k);
         }
 
         // Set a door
@@ -52,12 +54,87 @@ public class Building : MonoBehaviour
     void Update()
     {
         DebugSquare();
+        DebugPartition();
         foreach (List<Wall> walls in stages) {
             foreach (Wall w in walls) {
                 DebugWall(w);
             }
         }
+        //DebugArea();
     }
+
+    void InitSpace() {
+        RemoveSpace();
+
+        BuildArea();
+    }
+
+    void InitWalls() {
+        stages = new List<Wall>[height];
+        for (int k = 0; k < height; k++) {
+            BuildWall(k);
+        }
+    }
+
+    void RemoveSpace() {
+        List<RectInt> border = new List<RectInt>();
+        List<RectInt> inside = new List<RectInt>();
+        foreach (RectInt r in rects) {
+            if (r.xMin == 0 || r.yMin == 0 || r.xMax == width || r.yMax == length) {
+                border.Add(r);
+            }
+            else {
+                inside.Add(r);
+            }
+        }
+
+        // Delete some rectangles on the border
+        for (int k = 0; k < RectangleToRemove; k++) {
+            int idx = Random.Range(0, border.Count);
+            RectInt rect = border[idx];
+            Debug.Log(string.Format("Deleting rectangle {0}", rect));
+            border.RemoveAt(idx);
+
+            // Some rectangles from the inside are now on the border
+            for (int i = inside.Count - 1; i >= 0; i--) {
+                if (Area.TouchingEdges(rect, inside[i]).Count > 0) {
+                    border.Add(inside[i]);
+                    inside.RemoveAt(i);
+                }
+            }
+        }
+        rects = border;
+        rects.AddRange(inside);
+    }
+
+    void BuildWall(int h) {
+        List<Vector3> corners = new List<Vector3>();
+        //corners.Add(new Vector3(0, k, 0));
+        //corners.Add(new Vector3(width, k, 0));
+        //corners.Add(new Vector3(width, k, length));
+        //corners.Add(new Vector3(0, k, length));
+        for (int i = 0; i < area.Main.Count; i++) {
+            corners.Add(new Vector3(area.Main[i].x, h, area.Main[i].y));
+        }
+
+        stages[h] = new List<Wall>();
+        for (int i = 0; i < corners.Count - 1; i++) {
+            stages[h].Add(new Wall(corners[i], corners[i + 1]));
+        }
+        stages[h].Add(new Wall(corners[corners.Count - 1], corners[0]));
+    }
+
+    void BuildArea() {
+        // Build the area
+        area = new Area();
+        Debug.Log(string.Format("{0} rectangles remaining", rects.Count));
+        for (int i = 0; i < rects.Count; i++) {
+            area.Add(rects[i]);
+        }
+        Debug.Log(string.Format("area has {0} points", area.Main.Count));
+    }
+
+    #region Debug
 
     void DebugWall(Wall wall) {
         Debug.DrawLine(wall.start, wall.end, Color.blue);
@@ -100,4 +177,47 @@ public class Building : MonoBehaviour
         Debug.DrawLine(new Vector3(width, 0, length), new Vector3(width, height, length), Color.red);
         Debug.DrawLine(new Vector3(0, 0, length), new Vector3(0, height, length), Color.red);
     }
+
+    void DebugPartition() {
+        //Debug.Log(rects);
+        foreach (RectInt r in rects) {
+            Vector3 v1 = new Vector3(r.xMin, 0, r.yMin);
+            Vector3 v2 = new Vector3(r.xMin, 0, r.yMax);
+            Vector3 v3 = new Vector3(r.xMax, 0, r.yMax);
+            Vector3 v4 = new Vector3(r.xMax, 0, r.yMin);
+            Debug.DrawLine(v1, v2, Color.yellow);
+            Debug.DrawLine(v2, v3, Color.yellow);
+            Debug.DrawLine(v3, v4, Color.yellow);
+            Debug.DrawLine(v4, v1, Color.yellow);
+        }
+    }
+
+    void DebugArea() {
+
+        foreach (List<Vector2> points in area.Surfaces) {
+            //List<Vector2> points = area.Main;
+            for (int i = 0; i < points.Count - 1; i++) {
+                Vector3 p1 = new Vector3(points[i].x, 0, points[i].y);
+                Vector3 p2 = new Vector3(points[i + 1].x, 0, points[i + 1].y);
+                Debug.DrawLine(p1, p2, Color.magenta);
+                Debug.DrawLine(p1, p1 + Vector3.up * 0.5f, Color.magenta);
+            }
+            Debug.DrawLine(new Vector3(points[points.Count - 1].x, 0, points[points.Count - 1].y), new Vector3(points[0].x, 0, points[0].y), Color.magenta);
+            Debug.DrawLine(new Vector3(points[points.Count - 1].x, 0, points[points.Count - 1].y), new Vector3(points[points.Count - 1].x, 0.5f, points[points.Count - 1].y), Color.magenta);
+        }
+
+        if (m < rects.Count) {
+            RectInt r = rects[m];
+            Vector3 v1 = new Vector3(r.xMin, 0, r.yMin);
+            Vector3 v2 = new Vector3(r.xMin, 0, r.yMax);
+            Vector3 v3 = new Vector3(r.xMax, 0, r.yMax);
+            Vector3 v4 = new Vector3(r.xMax, 0, r.yMin);
+            Debug.DrawLine(v1, v2, Color.yellow);
+            Debug.DrawLine(v2, v3, Color.yellow);
+            Debug.DrawLine(v3, v4, Color.yellow);
+            Debug.DrawLine(v4, v1, Color.yellow);
+        }
+    }
+
+    #endregion
 }
