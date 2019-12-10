ed6045b2f3196ad4de0de2c068ad7bd689c3e9c0
cnis <cedric.nis@dbmail.com>
2019-12-10 17:08:52 +0100

    FieldOFView

---
+++ AI_Project/Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NPC
+{
+    public string name;
+    public int id;
+    
+
+    public abstract void OnUpdate();
+
+}
