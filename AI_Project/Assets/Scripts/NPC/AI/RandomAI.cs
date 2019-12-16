01beb8a556763e048daf592178e094b35ec25385
cnis <cedric.nis@dbmail.com>
2019-12-16 15:29:22 +0100

    NPC first step
    Abstract class NPC
    +
    RAndom AI

---
+++ AI_Project/Assets/Scripts/NPC/AI/RandomAI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandomAI : NPC
+{
+
+    [Range(2, 15)]
+    public float delay;
+    public int moveRange;
+
+    public override void Start()
+    {
+        base.Start();
+        StartCoroutine("Move", delay);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StopCoroutine("Move");
+        }
+
+    }
+
+
+    public override Vector3 UpdateMove()
+    {
+        Vector3 dst = new Vector3(Random.Range(-moveRange, moveRange), 0, Random.Range(-moveRange, moveRange));
+        Debug.Log(dst);
+        return dst;
+    }
+
+    IEnumerator Move(float delay)
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            agent.SetDestination(UpdateMove());
+            
+        }
+    }
+
+}
--- AI_Project/Assets/Scripts/NPC/NPC.cs
+++ AI_Project/Assets/Scripts/NPC/NPC.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public abstract class NPC
+[RequireComponent(typeof(Rigidbody), typeof(FieldOfView), typeof(NavMeshAgent))]
+public abstract class NPC : MonoBehaviour
 {
-    public string name;
+    public string myName;
     public int id;
-    
+    public float radiusCommunication;
 
-    public abstract void OnUpdate();
+    protected NavMeshAgent agent;
+    protected Rigidbody myRigidbody;
+    protected Vector3 destination;
+
+    public virtual void Start()
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+
+    public virtual Vector3 UpdateMove()
+    {
+        return new Vector3();
+    }
+
+    public virtual void NextAction()
+    {
+
+    }
 
 }
